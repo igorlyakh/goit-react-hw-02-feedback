d716d5d7a04a1f1ac6d256a48e1795de8f8cbcd7
Igor Liakh <igorlyah060@gmail.com>
2023-11-01 19:36:57 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'GlobalStyle';
-import 'normalizecss-3';
+import 'modern-normalize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
