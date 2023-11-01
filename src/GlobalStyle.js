8777b9fa4c536ebf2f44af70bdf8b8b8480be01b
Igor Liakh <igorlyah060@gmail.com>
2023-11-01 19:28:39 +0200

    added styles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+  img {
+    display: block;
+    max-width: 100%;
+  }
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`;
